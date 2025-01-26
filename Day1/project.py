a5eb8b6f28d34acae40a977cfc1c3a8e69906953
Prathm0025 <moreprathmesh849@gmail.com>
2025-01-26 22:29:12 +0530

    first commit

---
+++ Day1/project.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Project")
+city = input("What's the name of city you grew up?"+"\n")
+pet = input("What is your pet name?"+"\n")
+print("Your band name cloud be " '"'+city+" "+pet+" band"'"')
---
+++ Day1/variable-ex.py
@@ -0,0 +1,3 @@
+glass1 = "milk"
+glass2 = "juice"
+print("glass1 = "+glass2+ "\n" +"glass2 = " +glass1)
